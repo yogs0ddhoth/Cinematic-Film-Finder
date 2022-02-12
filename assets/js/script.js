2295c0008039ca93df841b4d34416e85cb027ba7
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-02-11 19:00:49 -0500

    fixed image rendering problem and added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-$('#search-form').on('click', 'button', function() {
+$('#search-form').on('click', 'button', function(event) {
   console.log('test');
+  event.preventDefault();
   let imdbSearchMovie = {
     "url": "https://imdb-api.com/en/API/AdvancedSearch/k_5yme52ms",
     "method": "GET",
@@ -10,37 +11,69 @@ $('#search-form').on('click', 'button', function() {
   let qParam = '?title=' + q + '&title_type=feature,tv_movie,tv_special,documentary,short';
   imdbSearchMovie.url += qParam;
   console.log(imdbSearchMovie);
-  $.ajax(imdbSearchMovie).done(function (response) {
-    let responseArray = response.results;
-    console.log(responseArray);
+  
+  // $.ajax(imdbSearchMovie).done(function (response) {
+  //   let responseArray = response.results;
+  //   console.log(responseArray);
     
-    //  Run through a for loop for rendering:
-    for (i = 0; i < 30; i++) {
-      // assign imdb id to image card as an id to be called with jquery
-      let imbdId = responseArray[i].id;
+  //   // set response to local storage so that rendering functionality can be tested without using limited api calls
+  //   localStorage.setItem(q, JSON.stringify(responseArray));
+    
+  //   //  Run through a for loop for rendering:
+  //   for (i = 0; i < 30; i++) {
+  //     // assign imdb id to image card as an id to be called with jquery
+  //     let imbdId = responseArray[i].id;
 
-      // render image with title, and year as a tag - styled as a card/thumbnail
-      let imdbDescrp = responseArray[i].description;
-      let imdbImage = responseArray[i].image;
-      let imdbTitle = responseArray[i].title;
+  //     // render image with title, and year as a tag - styled as a card/thumbnail
+  //     let imdbDescrp = responseArray[i].description;
+  //     let imdbImage = responseArray[i].image;
+  //     let imdbTitle = responseArray[i].title;
       
-      //* IMAGE NOT RENDERING CORRECTLY: CHECK BOOTSTRAP
-      let $card = $('<div></div>', {
-        'id': imbdId,
-        'class': 'card col',
-      }).appendTo('#results');
-      $('<img>', {
-        'src': imdbImage.replace('original', '192x264'),
-        'class': 'card-img-top',
-        'alt': imdbTitle,
-      }).appendTo($card);
-      $('<h2></h2>').text(imdbTitle + ' ' + imdbDescrp).appendTo($card);
-    }
-     
-     
-   
+  //     let $col = $('<div></div>', {
+  //       'class': 'col',
+  //     }).appendTo($('#results'));
+
+  //     let $card = $('<div></div>', {
+  //       'id': imbdId,
+  //       'class': 'card',
+  //     }).appendTo($col);
+  //     $('<img>', {
+  //       'src': imdbImage.replace('original', '192x264'),
+  //       'class': 'card-img-top',
+  //       'alt': imdbTitle,
+  //     }).appendTo($card);
+  //     $('<h2></h2>').text(imdbTitle + ' ' + imdbDescrp).appendTo($card);
+  //   }
+  // });
+  
+  // rendering functionality using local storage - for testing purposes
+  let responseArray = JSON.parse(localStorage.getItem(q));
+  //  Run through a for loop for rendering:
+  for (i = 0; i < 30; i++) {
+    // assign imdb id to image card as an id to be called with jquery
+    let imbdId = responseArray[i].id;
+
+    // render image with title, and year as a tag - styled as a card/thumbnail
+    let imdbDescrp = responseArray[i].description;
+    let imdbImage = responseArray[i].image;
+    let imdbTitle = responseArray[i].title;
     
-  });
+
+    let $col = $('<div></div>', {
+      'class': 'col',
+    }).appendTo($('#results'));
+
+    let $card = $('<div></div>', {
+      'id': imbdId,
+      'class': 'card',
+    }).appendTo($col);
+    $('<img>', {
+      'src': imdbImage.replace('original', '192x264'),
+      'class': 'card-img-top',
+      'alt': imdbTitle,
+    }).appendTo($card);
+    $('<h2></h2>').text(imdbTitle + ' ' + imdbDescrp).appendTo($card);
+  }
 })
 
 
