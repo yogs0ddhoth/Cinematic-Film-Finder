3738c896c4282cc5b638566199611488c7411b55
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-02-14 19:51:32 -0500

    styling revisions

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,76 +12,82 @@ $('#search-form').on('click', 'button', function(event) {
   imdbAdvancedSearch.url += qParam;
   console.log(imdbAdvancedSearch);
   
-  // //* IMPORTANT: IF THIS SECTION IS COMMENTED OUT, AND THERE ARE NO FETCH RESPONSES IN LOCAL STORAGE
-  // // THE CODE WILL NOT RUN PROPERLY
-  // $.ajax(imdbAdvancedSearch).done(function (response) {
-  //   let responseArray = response.results;
-  //   console.log(responseArray);
+  //* IMPORTANT: IF THIS SECTION IS COMMENTED OUT, AND THERE ARE NO FETCH RESPONSES IN LOCAL STORAGE
+  // THE CODE WILL NOT RUN PROPERLY
+  $.ajax(imdbAdvancedSearch).done(function (response) {
+    let responseArray = response.results;
+    console.log(responseArray);
     
-  //   // set response to local storage so that rendering functionality can be tested without using limited api calls
-  //   localStorage.setItem(q, JSON.stringify(responseArray));
+    // set response to local storage so that rendering functionality can be tested without using limited api calls
+    localStorage.setItem(q, JSON.stringify(responseArray));
     
-  //   //  Run through a for loop for rendering:
-  //   for (i = 0; i < responseArray.length; i++) {
-  //     // assign imdb id to image card as an id to be called with jquery
-  //     let imbdId = responseArray[i].id;
+    // clear previous search
+    $('#results').empty();
 
-  //     // render image with title, and year as a tag - styled as a card/thumbnail
-  //     let imdbDescrp = responseArray[i].description;
-  //     let imdbImage = responseArray[i].image;
-  //     let imdbTitle = responseArray[i].title;
-      
-  //     $('#results').empty();
+    //  Run through a for loop for rendering:
+    for (i = 0; i < responseArray.length; i++) {
+      // assign imdb id to image card as an id to be called with jquery
+      let imbdId = responseArray[i].id;
 
-  //     let $col = $('<div></div>', {
-  //       'class': 'col',
-  //     }).appendTo($('#results'));
+      // render image with title, and year as a tag - styled as a card/thumbnail
+      let imdbDescrp = responseArray[i].description;
+      let imdbImage = responseArray[i].image;
+      let imdbTitle = responseArray[i].title;
 
-  //     let $card = $('<div></div>', {
-  //       'id': imbdId,
-  //       'class': 'card',
-  //     }).appendTo($col);
-  //     $('<img>', {
-  //       'src': imdbImage.replace('original', '192x264'),
-  //       'class': 'card-img-top',
-  //       'alt': imdbTitle,
-  //     }).appendTo($card);
-  //     $('<h2></h2>').text(imdbTitle + ' ' + imdbDescrp).appendTo($card);
-  //     $('<button type="button" class="btn-btn-primary" data-toggle="modal" data-target="#infoModal"></button>').text('Info').appendTo($card);
-  //   }
-  // });
+      let $col = $('<div></div>', {
+        'class': 'col-2',
+        'style': 'margin-top:50px'
+      }).appendTo($('#results'));
+  
+      let $card = $('<div></div>', {
+        'id': imbdId,
+        'class': 'card h-100',
+        'style': 'background-color: transparent'
+      }).appendTo($col);
+
+      $('<img>', {
+        'src': imdbImage.replace('original', '480x660'),
+        'class': 'card-img-top',
+        'alt': imdbTitle,
+      }).appendTo($card);
+
+      $('<h2></h2>').text(imdbTitle + ' ' + imdbDescrp).appendTo($card);
+      $('<button type="button" class="btn-btn-primary" data-toggle="modal" data-target="#infoModal"></button>').text('Info').appendTo($card);
+    }
+  })
   
   
-  // rendering functionality using local storage - for testing purposes
-  let responseArray = JSON.parse(localStorage.getItem(q));
-  //  Run through a for loop for rendering:
-  for (i = 0; i < responseArray.length; i++) {
-    // assign imdb id to image card as an id to be called with jquery
-    let imbdId = responseArray[i].id;
+  // // rendering functionality using local storage - for testing purposes
+  // let responseArray = JSON.parse(localStorage.getItem(q));
+  // //  Run through a for loop for rendering:
+  // for (i = 0; i < responseArray.length; i++) {
+  //   // assign imdb id to image card as an id to be called with jquery
+  //   let imbdId = responseArray[i].id;
 
-    // render image with title, and year as a tag - styled as a card/thumbnail
-    let imdbDescrp = responseArray[i].description;
-    let imdbImage = responseArray[i].image;
-    let imdbTitle = responseArray[i].title;
+  //   // render image with title, and year as a tag - styled as a card/thumbnail
+  //   let imdbDescrp = responseArray[i].description;
+  //   let imdbImage = responseArray[i].image;
+  //   let imdbTitle = responseArray[i].title;
     
 
-    let $col = $('<div></div>', {
-      'class': 'col',
-    }).appendTo($('#results'));
+  //   let $col = $('<div></div>', {
+  //     'class': 'col-2',
+  //     'style': 'margin-top:50px'
+  //   }).appendTo($('#results'));
 
-    let $card = $('<div></div>', {
-      'id': imbdId,
-      'class': 'card h-100',
-      'style': 'background-color: transparent'
-    }).appendTo($col);
-    $('<img>', {
-      'src': imdbImage.replace('original', '480x660'),
-      'class': 'card-img-top',
-      'alt': imdbTitle,
-    }).appendTo($card);
-    $('<h2></h2>').text(imdbTitle + ' ' + imdbDescrp).appendTo($card);
-    $('<button type="button" class="btn-btn-primary" data-toggle="modal" data-target="#infoModal"></button>').text('Info').appendTo($card);
-  }
+  //   let $card = $('<div></div>', {
+  //     'id': imbdId,
+  //     'class': 'card h-100',
+  //     'style': 'background-color: transparent'
+  //   }).appendTo($col);
+  //   $('<img>', {
+  //     'src': imdbImage.replace('original', '480x660'),
+  //     'class': 'card-img-top',
+  //     'alt': imdbTitle,
+  //   }).appendTo($card);
+  //   $('<h2></h2>').text(imdbTitle + ' ' + imdbDescrp).appendTo($card);
+  //   $('<button type="button" class="btn-btn-primary" data-toggle="modal" data-target="#infoModal"></button>').text('Info').appendTo($card);
+  // }
 })
 
 
